834e3b67db3d3974fb492bfb890f51596ae96c78
jvens <jeramie.vens@gmail.com>
2016-06-16 19:38:34 +0000

    my changes

---
+++ test.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+curl --header "Authorization: key=AIzaSyC5acC8q53FvzJyym1VgM3FBbE6Any99f4" --header "Content-Type: application/json" https://android.googleapis.com/gcm/send -d @test.json
+
